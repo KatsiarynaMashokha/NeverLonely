5e136a24f8ed16bd02ff15391ada43086776f212
Kate Mashokha <katyamashokha@yahoo.com>
2017-10-14 20:24:47 -0700

    update a list of events when a new one added

--- app/src/main/java/com/epicodus/neverlonely/MainActivity.java
+++ app/src/main/java/com/epicodus/neverlonely/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -20,34 +21,23 @@ public class MainActivity extends AppCompatActivity{
     @Bind(R.id.events_list_view) ListView mEventsListView;
     @Bind(R.id.events_around_text_view) TextView mEventsAroundTextView;
     @Bind(R.id.add_fab) FloatingActionButton mAddFabButton;
-    private ArrayList<Event> events = new ArrayList<>();
+    public static ArrayList<Event> events = new ArrayList<>();
     private static final String EVENT_KEY = "EVENT";
+    EventsArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        if (events.isEmpty()) {
+            loadSavedData();
+        }
 
         Typeface grandHotelFont = Typeface.createFromAsset(getAssets(), "fonts/grandhotel.ttf");
         mEventsAroundTextView.setTypeface(grandHotelFont);
 
-        final EventsArrayAdapter adapter = new EventsArrayAdapter(this, events);
-        adapter.add(new Event("Friday happy hour", "Want to go for a happy hour to some bar in Greenwich Village",
-                "Friday, November 16",  "Greenwich Village", 5, "Andy Lee"));
-        adapter.add(new Event("Metropolitan museum visit", "There is a new exposition going on in a Brooklym museum. " +
-                "Would love to visit it",  "Thursday, November 22", "Brooklyn Museum", 3, "Kim Brown"));
-        adapter.add(new Event("Bowling at Brooklyn Bowl", "Tuesday, November 12"));
-        adapter.add(new Event("Central park afternoon walk", "Monday, November 12"));
-        adapter.add(new Event("Dog Walk in Prospect Park", "Friday, November 16"));
-        adapter.add(new Event("Skating at Rockfeller Center", "Thursday, November 22"));
-        adapter.add(new Event("Friday happy hour", "Friday, November 16"));
-        adapter.add(new Event("Metropolitan museum visit", "Thursday, November 22"));
-        adapter.add(new Event("Bowling at Brooklyn Bowl", "Tuesday, November 12"));
-        adapter.add(new Event("Central park afternoon walk", "Monday, November 12"));
-        adapter.add(new Event("Dog Walk in Prospect Park", "Friday, November 16"));
-        adapter.add(new Event("Skating at Rockfeller Center", "Thursday, November 22"));
-
+        adapter = new EventsArrayAdapter(this, events);
         mEventsListView.setAdapter(adapter);
         mEventsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -68,5 +58,42 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(newEventIntent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("KATE", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("KATE", "onStop");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("KATE", "onResume");
+        adapter.notifyDataSetChanged();
+    }
+
+    private void loadSavedData() {
+        events.add(new Event("Friday happy hour", "Want to go for a happy hour to some bar in Greenwich Village",
+                "Friday, November 16", "5:45pm", "Greenwich Village", 5, "Andy Lee"));
+        events.add(new Event("Metropolitan museum visit", "There is a new exposition going on in a Brooklym museum. " +
+                "Would love to visit it",  "Thursday, November 22", "11:30am", "Brooklyn Museum", 3, "Kim Brown"));
+        events.add(new Event("Bowling at Brooklyn Bowl", "Tuesday, November 12"));
+        events.add(new Event("Central park afternoon walk", "Monday, November 12"));
+        events.add(new Event("Dog Walk in Prospect Park", "Friday, November 16"));
+        events.add(new Event("Skating at Rockfeller Center", "Thursday, November 22"));
+        events.add(new Event("Friday happy hour", "Friday, November 16"));
+        events.add(new Event("Metropolitan museum visit", "Thursday, November 22"));
+        events.add(new Event("Bowling at Brooklyn Bowl", "Tuesday, November 12"));
+        events.add(new Event("Central park afternoon walk", "Monday, November 12"));
+        events.add(new Event("Dog Walk in Prospect Park", "Friday, November 16"));
+        events.add(new Event("Skating at Rockfeller Center", "Thursday, November 22"));
     }
 }
--- app/src/main/java/com/epicodus/neverlonely/NewEventActivity.java
+++ app/src/main/java/com/epicodus/neverlonely/NewEventActivity.java
@@ -1,13 +1,51 @@
 package com.epicodus.neverlonely;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class NewEventActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static com.epicodus.neverlonely.MainActivity.events;
+
+public class NewEventActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.save_button) Button mSaveButton;
+    @Bind(R.id.title_edit_text) EditText mTitleEditText;
+    @Bind(R.id.description_edit_text) EditText mDescriptionEditText;
+    @Bind(R.id.date_edit_text) EditText mDateEditText;
+    @Bind(R.id.time_edit_text) EditText mTimeEditText;
+    @Bind(R.id.location_edit_text) EditText mLocationEditText;
+    @Bind(R.id.max_attendees_edit_text) EditText mMaxAttendeesEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_event);
+        ButterKnife.bind(this);
+        mMaxAttendeesEditText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        mSaveButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String title = mTitleEditText.getText().toString();
+        mTitleEditText.getText().clear();
+        String description = mDescriptionEditText.getText().toString();
+        mDescriptionEditText.getText().clear();
+        String date = mDateEditText.getText().toString();
+        mDateEditText.getText().clear();
+        String time = mTimeEditText.getText().toString();
+        mTimeEditText.getText().clear();
+        String location = mLocationEditText.getText().toString();
+        mLocationEditText.getText().clear();
+        int attendees = Integer.parseInt(mMaxAttendeesEditText.getText().toString());
+        mMaxAttendeesEditText.getText().clear();
+        Event newEvent =  new Event(title, description, date, time, location, attendees, "null");
+        events.add(newEvent);
     }
 }
